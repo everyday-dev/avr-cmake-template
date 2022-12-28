768032d18e0188a4e9d3099e4f21d76aff4cefce
stephendpmurphy <stephendpmurphy@msn.com>
2022-12-28 14:43:35 -0500

    Remove tiny-oled header in main.c

--- src/main.c
+++ src/main.c
@@ -1,27 +1,3 @@
-/****************************************************************************
-    tiny-oled.firmware - A project to push the limits of my abilities and
-    understanding of embedded firmware development.
-    Copyright (C) 2020 Stephen Murphy - github.com/stephendpmurphy
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-****************************************************************************/
-
 /*! @file main.c
  * @brief Main source for the tiny-oled firmware
  */
@@ -29,7 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <util/delay.h>
+#include <avr/io.h>
+
+#define LED_STAT_DDR        (DDRD)
+#define LED_STAT_PORT       (PORTD)
+#define LED_STAT_PIN        (4)
 
 /*!
  * @brief Main function and entry point for the firmware
@@ -39,6 +19,8 @@
  * @return Returns void
  */
 int main(void) {
+    LED_STAT_DDR |= (1 << LED_STAT_PIN);
+    LED_STAT_PORT |= (1 << LED_STAT_PIN);
     while(1) {
     }
 }
