bc94b014ebd10e277330882a6e50c12207eee1c4
stephendpmurphy <stephendpmurphy@msn.com>
2022-12-27 21:06:14 -0500

    Initial push of project. README needs filled out and make targets tested

---
+++ src/main.c
@@ -0,0 +1,44 @@
+/****************************************************************************
+    tiny-oled.firmware - A project to push the limits of my abilities and
+    understanding of embedded firmware development.
+    Copyright (C) 2020 Stephen Murphy - github.com/stephendpmurphy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+****************************************************************************/
+
+/*! @file main.c
+ * @brief Main source for the tiny-oled firmware
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+/*!
+ * @brief Main function and entry point for the firmware
+ *
+ * @param[in] void
+ *
+ * @return Returns void
+ */
+int main(void) {
+    while(1) {
+    }
+}
