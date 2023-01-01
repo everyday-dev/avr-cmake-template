f9f97f17fd5b625c094d37c5a0accf6b63ac12ee
stephendpmurphy <stephendpmurphy@msn.com>
2023-01-01 18:02:33 -0500

    Added source versioning and unit testing to CI

---
+++ test/test_example_test.c
@@ -0,0 +1,18 @@
+#ifdef TEST
+
+#include "unity.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_example_test_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement example_test");
+}
+
+#endif // TEST
